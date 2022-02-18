271bdd3a8dbd87563d28e71a2f0f35fae3eacbbb
jakmum <jakobmummert@gmail.com>
2022-02-18 20:57:58 +0100

    first somewhat playable version of new AI

--- OthelloAI.java
+++ OthelloAI.java
@@ -1,32 +1,44 @@
 public class OthelloAI implements IOthelloAI {
 
-
     @Override
     public Position decideMove(GameState s) {
         return maxValue(s).getMove();
     }
 
     private Value maxValue(GameState s) {
-        if (s.isFinished()) return new Value(utility(s), null); 
-        double utility = Double.NEGATIVE_INFINITY;//i'm not sure about this one
-        Value resultValue = new Value((int)utility, null);
+        if (s.isFinished())
+            return new Value(utility(s), null);
+        double utility = Double.NEGATIVE_INFINITY;// i'm not sure about this one
+        Value resultValue = new Value((int) utility, null);
 
-        for (Position move : s.legalMoves()) { 
-            Value tempValue = minValue(result(s,move));       
-            if (resultValue.getUtility() < tempValue.getUtility()) resultValue = new Value(tempValue.getUtility(), move);
+        for (Position move : s.legalMoves()) {
+            GameState newState = new GameState(s.getBoard(), s.getPlayerInTurn());
+            if (newState.insertToken(move)) {
+                newState.changePlayer();
+                Value tempValue = minValue(newState);
+                if (resultValue.getUtility() < tempValue.getUtility())
+                    resultValue = new Value(tempValue.getUtility(), move);
+            }
         }
+
+        System.out.println("move utility: " + resultValue.getUtility());
         return resultValue;
     }
 
-
     private Value minValue(GameState s) {
-        if (s.isFinished()) return new Value(utility(s), null);
-        double utility = Double.POSITIVE_INFINITY;//i'm not sure about this one
-        Value resultValue = new Value((int)utility, null);
+        if (s.isFinished())
+            return new Value(utility(s), null);
+        double utility = Double.POSITIVE_INFINITY;// i'm not sure about this one
+        Value resultValue = new Value((int) utility, null);
 
-        for (Position a : s.legalMoves()) {
-            Value tempValue = maxValue(result(s,a));       
-            if (resultValue.getUtility() > tempValue.getUtility()) resultValue = new Value(tempValue.getUtility(), a);
+        for (Position move : s.legalMoves()) {
+            GameState newState = new GameState(s.getBoard(), s.getPlayerInTurn());
+            if (newState.insertToken(move)) {
+                newState.changePlayer();
+                Value tempValue = maxValue(newState);
+                if (resultValue.getUtility() > tempValue.getUtility())
+                    resultValue = new Value(tempValue.getUtility(), move);
+            }
         }
         return resultValue;
     }
@@ -35,13 +47,8 @@ public class OthelloAI implements IOthelloAI {
         return null;
     }
 
- 
-
     private int utility(GameState s) {
-        return 0;
+        return s.countTokens()[0];
     }
 
-
-    
-    
 }
